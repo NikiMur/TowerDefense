ddb4bd47f9c884cf3547857fd2c3ef93f2e0170b
NikiMur <zik.9507@bk.ru>
2022-07-22 20:34:36 +0300

    Asset

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MainMenu : MonoBehaviour
+{
+   
+  public void ExitGame()
+    {
+        Debug.Log("Игра закрылась");
+        Application.Quit();
+    }
+}
